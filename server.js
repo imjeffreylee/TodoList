933c51326df8e86488c9108e3318ccead2476698
imjeffreylee <imjeffreylee@gmail.com>
2023-09-03 02:20:12 +0800

    chore: configs for deploy

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+const LOCALHOST = 'mongodb://localhost:27017/TodoList';
+const DEPLOYED = process.env.MONGODB_URI;
 
 mongoose
-  .connect('mongodb://localhost:27017/TodoList', {
+  .connect(DEPLOYED, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -18,4 +24,10 @@ mongoose
 const todosRouter = require('./routes/todo');
 app.use('/todos', todosRouter);
 
-app.listen(3001, () => console.log('Server is running on port 3001'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3001, () =>
+  console.log('Server is running on port 3001')
+);
